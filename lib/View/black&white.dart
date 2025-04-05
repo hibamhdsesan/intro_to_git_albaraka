ffa34eda2d249bfe446d4e4c0570cfa741fd01b9
hiba <hibamhdsesan@gmail.com>
2025-04-05 22:44:00 +0300

    second version in git

--- lib/View/black&white.dart
+++ lib/View/black&white.dart
@@ -69,6 +69,7 @@ class Black_White_Screen extends StatelessWidget {
             ProductListView(),
             ProductListView(),
             ProductListView(),
+            
           ],
         ));
   }
